79d797f2b188a1e9a23f1948bbee73c47b7b7920
anonymous anonymous <anum.mithani@kellogg.northwestern.edu>
2022-03-09 19:58:07 +0000

    Whitelist columns in Sight for ActiveAdmin

--- app/admin/sights.rb
+++ app/admin/sights.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Sight do
 
+ permit_params :name, :date, :time, :trip_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
