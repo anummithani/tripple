e0be2af7198f48b732956fb7dc8686d8b14f9ba0
anonymous anonymous <anum.mithani@kellogg.northwestern.edu>
2022-03-09 19:59:21 +0000

    Generate graphiti resources for trips

---
+++ app/resources/trip_resource.rb
@@ -0,0 +1,16 @@
+class TripResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :city, :string
+  attribute :country, :string
+  attribute :trip_name, :string
+  attribute :trip_image, :string
+  attribute :arrival_date, :date
+  attribute :departure_date, :date
+
+  # Direct associations
+
+  # Indirect associations
+
+end
